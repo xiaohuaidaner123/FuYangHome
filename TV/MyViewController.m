699e153d0f41c2f11aba5cf046d1a00f958462fe
iking <iking@ikingdeiMac.local>
2017-03-27 13:21:18 +0800

    lklg

--- TV/MyViewController.m
+++ TV/MyViewController.m
@@ -26,6 +26,7 @@
     [super viewWillAppear:animated];
     [self.navigationController.navigationBar setBackgroundImage:[UIImage createImageWithColor:RGB(68, 202, 181)] forBarMetrics:UIBarMetricsDefault];
     [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
+    
 }
 
 - (void)viewDidLoad {
